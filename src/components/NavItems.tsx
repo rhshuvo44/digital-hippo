fd54cc679d1152bd11ba9e4da1a9b929c312c276
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2023-12-03 12:26:57 +0600

    navbar

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,12 +1,15 @@
 "use client";
 import { PRODUCT_CATEGORIES } from "@/config";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/useOnClickOutside";
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
   const isAnyOpen = activeIndex !== null;
+  const navRef = useRef<HTMLDivElement | null>(null);
+  useOnClickOutside(navRef, () => setActiveIndex(null));
   return (
-    <div className="flex gap-4 h-full">
+    <div className="flex gap-4 h-full" ref={navRef}>
       {PRODUCT_CATEGORIES.map((category, i) => {
         const handleOpen = () => {
           if (activeIndex === i) {
