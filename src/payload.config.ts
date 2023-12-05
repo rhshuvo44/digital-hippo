7081f4b36ba760037528f24f3e3500d955496d5a
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2023-12-05 17:02:40 +0600

    server added

---
+++ src/payload.config.ts
@@ -0,0 +1,36 @@
+import { webpackBundler } from "@payloadcms/bundler-webpack";
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { slateEditor } from "@payloadcms/richtext-slate";
+import dotenv from "dotenv";
+import path from "path";
+import { buildConfig } from "payload/config";
+
+dotenv.config({
+  path: path.resolve(__dirname, "../.env"),
+});
+
+export default buildConfig({
+  serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL || "",
+  collections: [],
+  routes: {
+    admin: "/sell",
+  },
+  admin: {
+    bundler: webpackBundler(),
+    meta: {
+      titleSuffix: "- DigitalHippo",
+      favicon: "/favicon.ico",
+      ogImage: "/thumbnail.jpg",
+    },
+  },
+  rateLimit: {
+    max: 2000,
+  },
+  editor: slateEditor({}),
+  db: mongooseAdapter({
+    url: process.env.MONGODB_URL!,
+  }),
+  typescript: {
+    outputFile: path.resolve(__dirname, "payload-types.ts"),
+  },
+});
