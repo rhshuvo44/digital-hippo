5f7dc290845bce7ac1d6874bef47017bdb994565
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2023-12-06 18:18:44 +0600

    email verification pages

--- src/app/(auth)/verify-email/pages.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const VerifyEmailPage = () => {
-  return <div></div>;
-};
-
-export default VerifyEmailPage;
--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -2,6 +2,7 @@ import { AuthCredentialsValidator } from "../lib/validators/account-credentials-
 import { TRPCError } from "@trpc/server";
 import { publicProcedure, router } from "./trpc";
 import { getPayloadClient } from "../get-payload";
+import { z } from "zod";
 
 export const authRouter = router({
   createPayloadUser: publicProcedure
@@ -31,5 +32,19 @@ export const authRouter = router({
         },
       });
       return { success: true, sentToEmail: email };
+    }),
+  verifyEmail: publicProcedure
+    .input(z.object({ token: z.string() }))
+    .query(async ({ input }) => {
+      const { token } = input;
+      const payload = await getPayloadClient();
+      const isVerified = await payload.verifyEmail({
+        collection: "users",
+        token,
+      });
+      if (!isVerified) {
+        throw new TRPCError({ code: "UNAUTHORIZED" });
+      }
+      return { succes: true };
     }),
 });
